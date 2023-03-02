6c26cdc3a16da1052c4852b996c4039e1ba2d489
Anton <nachalnikkochengui@gmail.com>
2023-03-02 15:16:04 +0800

    3zagachi

---
+++ cYbe/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число: ");
+int kyb = Convert.ToInt32(Console.ReadLine());
+
+void CYbe(int[] cubkey)
+{
+  int index = 0;
+  int length = cubkey.Length;
+  while (index <  length){
+    cubkey[index] = Convert.ToInt32(Math.Pow(index, 3));
+    index++;
+  }
+}
+
+void PrintArry(int[] tell)
+{
+  int length = tell.Length;
+  int index = 0;
+  while(index < length){
+    Console.Write(tell[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[kyb+1];
+CYbe(arry);
+PrintArry(arry);
